89868fd7b47e57980b1815a266618e45ea927ccf
ktalha <66550263+ktalha@users.noreply.github.com>
2020-07-19 21:34:03 +0100

    Create app.js

---
+++ 1.1. JS Intro and React Overview/D- Project: Dog Years/app.js
@@ -0,0 +1,27 @@
+// variable pressenting my age
+let myAge = 27;
+
+// variable earlyYears
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// substract 2 from my age
+myAge -= 2;
+
+// store myAge in a variable called laterYears
+let laterYears = myAge;
+
+// multiply laterYears with 4
+laterYears *=4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+// Add earlyYears and laterYears together
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Kady'.toLowerCase();
+// console.log(myName.toLowerCase());
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
+
+
