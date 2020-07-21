1366eab3452a34cb2f694095173c13ceb2460b94
ktalha <66550263+ktalha@users.noreply.github.com>
2020-07-21 19:18:28 +0100

    Update 5-airplane.js

--- 4.1. Modules/A- LESSON: Intermediate JS Modules/5-airplane.js
+++ 4.1. Modules/A- LESSON: Intermediate JS Modules/5-airplane.js
@@ -1,23 +1,37 @@
-let availableAirplanes = [{
- name: 'AeroJet',
- fuelCapacity: 800,
- availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
-}, 
-{name: 'SkyJet',
- fuelCapacity: 500,
- availableStaff: ['pilots', 'flightAttendants']
-}];
+// let availableAirplanes = [{
+//  name: 'AeroJet',
+//  fuelCapacity: 800,
+//  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+// }, 
+// {name: 'SkyJet',
+//  fuelCapacity: 500,
+//  availableStaff: ['pilots', 'flightAttendants']
+// }];
 
-let flightRequirements = {
-  requiredStaff: 4,
-};
+// let flightRequirements = {
+//   requiredStaff: 4,
+// };
 
-function meetsStaffRequirements(availableStaff, requiredStaff) {
-  if (availableStaff.length >= requiredStaff) {
-    return true;
-  } else {
-    return false;
-  }
-};
+// function meetsStaffRequirements(availableStaff, requiredStaff) {
+//   if (availableStaff.length >= requiredStaff) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 
-export { availableAirplanes, flightRequirements, meetsStaffRequirements};
+// export { availableAirplanes, flightRequirements, meetsStaffRequirements};
+
+let Airplane = {};
+
+Airplane.availableAirplanes = [
+{
+  name: 'AeroJet',
+  fuelCapacity: 800
+ }, 
+ {name: 'SkyJet',
+  fuelCapacity: 500
+ }
+];
+
+export default Airplane;
